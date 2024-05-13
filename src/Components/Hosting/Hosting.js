6c24ff4d16eaaf26d382e46f9b86e6a15fb2f950
yahya-fk <fekyah0@gmail.com>
2024-05-13 21:18:24 +0100

    Final Commit

--- src/Components/Hosting/Hosting.js
+++ src/Components/Hosting/Hosting.js
@@ -1,8 +1,24 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import LocalForm from "./LocalForm";
+import Cookies from "js-cookie";
+import {useNavigate} from "react-router-dom";
 
 const Hosting = () => {
+  const [cookieValue] = useState(Cookies.get('UserId') || '');
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    const cookieValue = Cookies.get('UserId');
+    if (cookieValue) {
+      if (cookieValue === '') {
+        console.log("Cookie 'UserId' exists with value:", cookieValue);
+        navigate('/Auth/Login');
+      }
+    }
+    else {
+      navigate('/Auth/Login');
+    }
+  }, [cookieValue]);
   return (<LocalForm/>);
 
 }
